aaf5e5a93ba721502f90184dc21bacab69f26bb5
Nicolas Phister <nicolas@phister.fr>
2017-12-06 11:05:13 +0100

    fixed coding style in BaseModule

--- mediator/BaseModule.cpp
+++ mediator/BaseModule.cpp
@@ -1,31 +1,24 @@
 #include "Module.hpp"
-#include "Mediator.hpp"
 
-namespace putils
-{
-    BaseModule::BaseModule(Mediator *mediator)
-            : _mediator(mediator)
-    {
+namespace putils {
+    BaseModule::BaseModule(Mediator * mediator)
+            : _mediator(mediator) {
     }
 
-    BaseModule::~BaseModule()
-    {
+    BaseModule::~BaseModule() {
         if (_mediator)
             _mediator->removeModule(*this);
     }
 
-    void BaseModule::sendDataPacket(const ADataPacket &packet) const noexcept
-    {
+    void BaseModule::sendDataPacket(const ADataPacket & packet) const noexcept {
         _mediator->sendDataPacket(packet);
     }
 
-    void BaseModule::sendDataPacketTo(const ADataPacket &packet, BaseModule &dest) const noexcept
-    {
+    void BaseModule::sendDataPacketTo(const ADataPacket & packet, BaseModule & dest) const noexcept {
         dest.receive(packet);
     }
 
-    void BaseModule::runTask(const std::function<void()> &f) const
-    {
+    void BaseModule::runTask(const std::function<void()> & f) const {
         _mediator->runTask(f);
     }
 }
