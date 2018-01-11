0c88c2a530b76465e10dd2be0b2d0facab666075
Nicolas Phister <nicolas@phister.fr>
2018-01-11 12:03:12 +0100

    CS in rand

--- rand.hpp
+++ rand.hpp
@@ -6,15 +6,12 @@
 namespace putils {
     template<typename T>
     inline T rand(T start = 0, T end = std::numeric_limits<T>::max) noexcept {
-        if constexpr (std::is_floating_point<T>::value)
-                {
-                        static std::random_device r;
-                        static std::default_random_engine e(r());
-                        std::uniform_real_distribution<T> d(start, end);
-                        return d(e);
-                }
-        else
-        {
+        if constexpr (std::is_floating_point<T>::value) {
+            static std::random_device r;
+            static std::default_random_engine e(r());
+            std::uniform_real_distribution<T> d(start, end);
+            return d(e);
+        } else {
             static std::random_device r;
             static std::default_random_engine e(r());
             static std::uniform_int_distribution<int> d;
